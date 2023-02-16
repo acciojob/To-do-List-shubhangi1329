c43ea9020b0b8897bc203bdee01ae8005099ce70
shubhangi1329 <117506539+shubhangi1329@users.noreply.github.com>
2023-02-16 15:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
 
+function Todo(){
+	var todoitem=document.querySelector('input').value;
+	var btn=document.querySelector('button');
+	var ol=document.querySelector('ol');
+	if(todoitem){
+		var li=document.createElement('li');
+		li.textContent=todoitem;
+		ol.appendChild(li);
+		todoitem="";
+		
+	}
+}
+btn.addEventListener('click',Todo)
+	
